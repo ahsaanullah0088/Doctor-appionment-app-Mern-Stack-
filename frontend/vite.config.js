c6a1064f06fe55e15b8ab0a9052635977b5404db
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-10-05 14:38:02 +0500

    changes for frontend deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
-  plugins: [react(),
-     tailwindcss(),
-  ],
-  server:{port:5173}
+  plugins: [react(), tailwindcss()],
+  server: { port: 5173 },
+  build: {
+    outDir: 'dist', // 👈 Explicitly tell Vite where to output build files
+  },
 })
